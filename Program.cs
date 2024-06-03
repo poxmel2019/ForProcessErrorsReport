883e106c4f05b67f621c2865832002f86c013e40
poxmel2019 <46764191+poxmel2019@users.noreply.github.com>
2024-06-03 18:08:18 +0500

    Added month, year and folder

--- Program.cs
+++ Program.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.Data.SqlClient;
 using Spire.Xls;
-using System.Runtime.ExceptionServices;
 
 namespace ForProcessErrors
 {
@@ -8,12 +7,52 @@ namespace ForProcessErrors
     {
         static async Task Main(string[] args)
         {
-            Workbook workbook = new Workbook();
-            Worksheet worksheet = workbook.Worksheets[0];
-            worksheet.Name = "Report";
 
-            Worksheet worksheet2 = workbook.Worksheets[1];
-            worksheet2.Name = "ResultCodes";
+            Dictionary<int, string> months = new Dictionary<int, string>()
+            {
+                {1, "january"},
+                {2, "february"},
+                {3, "march"},
+                {4, "april"},
+                {5, "may"},
+                {6, "june"},
+                {7, "july"},
+                {8, "august"},
+                {9, "september"},
+                {10, "october"},
+                {11, "november"},
+                {12, "december"}
+            };
+
+            int year = DateProcessing("y");
+            Console.WriteLine(year);
+
+            int month = DateProcessing("m");
+            Console.WriteLine(month);
+
+            //string month_name = new DateTime(year, month, 1).ToString("MMMM").ToLower(); // ru //?
+            string month_name = months[month];
+
+            Console.WriteLine(month_name);
+
+            string address = "C:\\for_work\\db\\other\\three_important_reports\\errors_by_processes\\";
+            string file = $"error_{month_name}_{year}";
+            string dir = $"{address}{file}";
+
+            if (!Directory.Exists(dir)) 
+            {
+                Directory.CreateDirectory(dir);
+            }
+
+
+
+
+            Workbook workbook = new Workbook();
+            Worksheet worksheet_first = workbook.Worksheets[0];
+            worksheet_first.Name = "Report";
+
+            Worksheet worksheet_second = workbook.Worksheets[1];
+            worksheet_second.Name = "ResultCodes";
                 
             string connectionString = "Server=hbmssqltest.halykbank.nb;" +
                 "Database=CorePayments;" +
@@ -21,40 +60,41 @@ namespace ForProcessErrors
                 "Password=0coayiwbYVReR;" +
                 "TrustServerCertificate=true;";
 
-            // all
+            // querries
             // 1
             string all_processes = "select \r\ncount(*) all_processes\r\n" +
      "from processes p (nolock)\r\n" +
      "where StartDate between\r\n" +
-     "cast('2024-04-01' as date) \r\n" +
+     $"cast('{year}-{month-1}-01' as date) \r\n" +
      "and \r\n" +
-     "cast('2024-05-01' as date)\r\n;";
+     $"cast('{year}-{month}-01' as date)\r\n;";
             // 2
             string all_success_processes = "select \r\ncount(*)\r\n " +
                 "from processes p (nolock)\r\n" +
                 "where StartDate " +
-                "between\r\ncast('2024-04-01' as date) \r\n" +
-                "and \r\ncast('2024-05-01' as date)\r\n" +
+                $"between\r\ncast('{year}-{month-1}-01' as date) \r\n" +
+                $"and \r\ncast('{year}-{month}-01' as date)\r\n" +
                 "and LastPortalServiceOperationStateId in (16,37)\r\n;";    //81
             // 3
             string all_fail_process_exec = "select \r\ncount(*)\r\n" +
                 "from processes p (nolock)\r\n" +
-                "where StartDate between\r\ncast('2024-04-01' as date) \r\n" +
-                "and \r\ncast('2024-05-01' as date)\r\n" +
+                "where StartDate " +
+                $"between\r\ncast('{year}-{month-1}-01' as date) \r\n" +
+                $"and \r\ncast('{year}-{month}-01' as date)\r\n" +
                 "and LastPortalServiceOperationStateId in (10,35)\r\n;";
             // 4
             string all_success_exec_fail_callback = "select \r\ncount(*)\r\n" +
                 "from processes p (nolock)\r\n" +
                 "where StartDate between\r\n" +
-                "cast('2024-04-01' as date) \r\n" +
-                "and \r\ncast('2024-05-01' as date)\r\n" +
+                $"cast('{year}-{month-1}-01' as date) \r\n" +
+                $"and \r\ncast('{year}-{month}-01' as date)\r\n" +
                 "and LastPortalServiceOperationStateId in (9, 34)\r\n;";
             // 5
             string all_fail_process_callback = "select \r\ncount(*)\r\n" +
                 "from processes p (nolock)\r\n" +
                 "where StartDate " +
-                "between\r\ncast('2024-04-01' as date) \r\n" +
-                "and \r\ncast('2024-05-01' as date)\r\n" +
+                $"between\r\ncast('{year}-{month-1}-01' as date) \r\n" +
+                $"and \r\ncast('{year}-{month}-01' as date)\r\n" +
                 "and LastPortalServiceOperationStateId in (17,38)\r\n;";
 
             // payment
@@ -62,37 +102,38 @@ namespace ForProcessErrors
             string payment_success_processes = "select \r\ncount(*)\r\n" +
                 "from processes p (nolock)\r\n" +
                 "where StartDate between\r\n" +
-                "cast('2024-04-01' as date) \r\n" +
-                "and \r\ncast('2024-05-01' as date)\r\n" +
+                $"cast('{year}-{month-1}-01' as date) \r\n" +
+                $"and \r\ncast('{year}-{month}-01' as date)\r\n" +
                 "and LastPortalServiceOperationStateId in (16)\r\n;";
             // 7
             string payment_fail_check = "select \r\ncount(*) fail_check\r\n" +
                 "from processes p (nolock)\r\n" +
                 "where StartDate between\r\n" +
-                "cast('2024-04-01' as date) \r\n" +
-                "and \r\ncast('2024-05-01' as date)\r\n" +
+                $"cast('{year}-{month-1}-01' as date) \r\n" +
+                $"and \r\ncast('{year}-{month}-01' as date)\r\n" +
                 "and LastPortalServiceOperationStateId in (13)\r\n;";
             // 8
             string payment_fail_exec = "select \r\ncount(*) fail_exec" +
                 "\r\nfrom processes p (nolock)\r\n" +
-                "where StartDate between\r\ncast('2024-04-01' as date) \r\n" +
-                "and \r\ncast('2024-05-01' as date)\r\n" +
+                "where StartDate " +
+                $"between\r\ncast('{year}-{month-1}-01' as date) \r\n" +
+                $"and \r\ncast('{year}-{month}-01' as date)\r\n" +
                 "and LastPortalServiceOperationStateId in (10)\r\n;";
             // 9
             string payment_succes_exec_fail_callback = "select \r\n" +
                 "count(*) no_callback\r\n" +
                 "from processes p (nolock)\r\n" +
                 "where StartDate between\r\n" +
-                "cast('2024-04-01' as date) \r\n" +
-                "and \r\ncast('2024-05-01' as date)\r\n" +
+                $"cast('{year}-{month-1}-01' as date) \r\n" +
+                $"and \r\ncast('{year}-{month}-01' as date)\r\n" +
                 "and LastPortalServiceOperationStateId = 9";
             // 10
             string payment_fail_callback = "select \r\ncount(*) " +
                 "fail_callback\r\n" +
                 "from processes p (nolock)\r\n" +
                 "where StartDate between\r\n" +
-                "cast('2024-04-01' as date) \r\n" +
-                "and \r\ncast('2024-05-01' as date)\r\n" +
+                $"cast('{year}-{month-1}-01' as date) \r\n" +
+                $"and \r\ncast('{year}-{month}-01' as date)\r\n" +
                 "and LastPortalServiceOperationStateId in (17)";
 
             // tranfer
@@ -101,30 +142,31 @@ namespace ForProcessErrors
                 "success\r\n" +
                 "from processes p (nolock)\r\n" +
                 "where StartDate " +
-                "between\r\ncast('2024-04-01' as date) \r\n" +
-                "and \r\ncast('2024-05-01' as date)\r\nand LastPortalServiceOperationStateId in (37)";
+                $"between\r\ncast('{year}-{month-1}-01' as date) \r\n" +
+                $"and \r\ncast('{year}-{month}-01' as date)\r\n" +
+                "and LastPortalServiceOperationStateId in (37)";
             // 12
             string transfer_fail_exec = "select \r\ncount(*) fail_exec" +
                 "\r\nfrom processes p (nolock)\r\n" +
                 "where StartDate " +
-                "between\r\ncast('2024-04-01' as date) \r\n" +
-                "and \r\ncast('2024-05-01' as date)\r\n" +
+                $"between\r\ncast('{year}-{month-1}-01' as date) \r\n" +
+                $"and \r\ncast('{year}-{month}-01' as date)\r\n" +
                 "and LastPortalServiceOperationStateId in (35)";
             // 13
             string transfer_success_exec = "select \r\ncount(*) " +         //81
                 "no_callback\r\n" +
                 "from processes p (nolock)\r\n" +
                 "where StartDate between\r\n" +
-                "cast('2024-04-01' as date) \r\n" +
-                "and \r\ncast('2024-05-01' as date)\r\n" +
+                $"cast('{year}-{month-1}-01' as date) \r\n" +
+                $"and \r\ncast('{year}-{month}-01' as date)\r\n" +
                 "and LastPortalServiceOperationStateId in (34)\r\n;";
             // 14
             string transfer_fail_callback = "select \r\ncount(*) " +        
                 "fail_callback\r\n" +
                 "from processes p (nolock)\r\n" +
                 "where StartDate between\r\n" +
-                "cast('2024-04-01' as date) \r\n" +
-                "and \r\ncast('2024-05-01' as date)\r\n" +
+                $"cast('{year}-{month-1}-01' as date) \r\n" +
+                $"and \r\ncast('{year}-{month}-01' as date)\r\n" +
                 "and LastPortalServiceOperationStateId = 38;";
 
             // 15
@@ -132,8 +174,8 @@ namespace ForProcessErrors
                 "count(resultcode) co\r\n" +
                 "from ProcessOperations (nolock)\r\n" +
                 "where StartDate between\r\n" +
-                "cast('2024-04-01' as date) \r\n" +
-                "and \r\ncast('2024-05-01' as date)\r\n" +
+                $"cast('{year}-{month-1}-01' as date) \r\n" +
+                $"and \r\ncast('{year}-{month}-01' as date)\r\n" +
                 "and ResultCode < 0\r\ngroup by ResultCode\r\n" +
                 "order by ResultCode desc";
 
@@ -187,29 +229,25 @@ namespace ForProcessErrors
             {
                 await connection.OpenAsync();
 
-                SqlCommand command = new SqlCommand(sqlExpressions[0], connection);
-                SqlDataReader reader = await command.ExecuteReaderAsync();
-
-                await reader.CloseAsync();
-
-
                 for (int i = 0; i < sqlExpressions.Length; i++)
                 {
-                    command = new SqlCommand(sqlExpressions[i], connection);
-                    reader = await command.ExecuteReaderAsync();
+                    SqlCommand command = new SqlCommand(sqlExpressions[i], connection);
+                    SqlDataReader reader = await command.ExecuteReaderAsync();
 
                     if (reader.HasRows)
                     {
-
-                        worksheet.Range[i+1, 1].Value = titles[i];
-                        
+                        worksheet_first.Range[i + 1, 1].Value = titles[i];
                     }
 
                     int j = i+1;
                     while (await reader.ReadAsync())
                     {
                         object result = reader.GetValue(0);
-                        worksheet.Range[i+1, 2].Value = result.ToString();
+                        if (reader.HasRows)
+                        {
+                            worksheet_first.Range[i + 1, 2].Value = result.ToString();
+                        }
+                        
                     }
 
                     await reader.CloseAsync();
@@ -222,50 +260,130 @@ namespace ForProcessErrors
                 style.Font.IsBold = true;
                 for (int i = 1; i <= sqlExpressions.Length; i++)
                 {
-                    worksheet.Range[i, 1, i, 1].Style = style;
+                    worksheet_first.Range[i, 1, i, 1].Style = style;
                 }
 
-
                 SqlCommand command2 = new SqlCommand(resultCodes, connection);
-                SqlDataReader reader2 = await command2.ExecuteReaderAsync();
+                SqlDataReader reader_second = await command2.ExecuteReaderAsync();
 
-                worksheet2.Range[1, 1].Value = "Код";
-                worksheet2.Range[1, 2].Value = "Количество";
+                worksheet_second.Range[1, 1].Value = "Код";
+                worksheet_second.Range[1, 2].Value = "Количество";
 
                 int k = 2;
-                while (await reader2.ReadAsync())
+                while (await reader_second.ReadAsync())
                 {
-                    object resultCode = reader2.GetValue(0);
-                    object quantity = reader2.GetValue(1);
+                    object resultCode = reader_second.GetValue(0);
+                    object quantity = reader_second.GetValue(1);
 
-                    worksheet2.Range[k, 1].Value = resultCode.ToString();
-                    worksheet2.Range[k, 2].Value = quantity.ToString();
+                    worksheet_second.Range[k, 1].Value = resultCode.ToString();
+                    worksheet_second.Range[k, 2].Value = quantity.ToString();
                     k++;
                 }
 
+                await reader_second.CloseAsync();
+                await connection.CloseAsync();
+
                 Console.WriteLine("Result code finished");
 
-                worksheet2.Range[1, 1, 1, 2].Style = style;
-
-                await reader2.CloseAsync();
-
-
-
+                worksheet_second.Range[1, 1, 1, 2].Style = style;
                 // fit width of columns
-                worksheet.AllocatedRange.AutoFitColumns();
+                worksheet_first.AllocatedRange.AutoFitColumns();
 
                 Console.WriteLine("Excel file finished");
 
                 // save to excel file
-                workbook.SaveToFile("C:\\for_work\\code\\my_projects\\ForExcel\\ReportError.xlsx",ExcelVersion.Version2016);
+                try
+                {
+                    workbook.SaveToFile($"{dir}\\{file}.xlsx", ExcelVersion.Version2016);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+                }
+                
 
-       
             }
 
             Console.Read();
 
+        }
+
+        static int DateProcessing(string type)
+        {
+            string date_string, date_name;
+            int date_int = 0;
+
+            if (type == "y")
+            {
+                Console.WriteLine("Year:");
+            }
+
+            //else if (type == "m") 
+            else
+            {
+                Console.WriteLine("Month:");
+            }
 
 
+            // year
+            while (true)
+            {
+                date_string = Console.ReadLine();
+                // check for void
+                if (string.IsNullOrEmpty(date_string))
+                {
+                    if (type == "y")
+                    {
+                        date_int = DateTime.Now.Year;
+                    }
+                    else if (type == "m")
+                    {
+                        date_int = DateTime.Now.Month;
+                    }
+
+                    break;
+                }
+                else
+                {   // check for number
+                    bool isNumeric = int.TryParse(date_string, out date_int);
+                    if (isNumeric)
+                    {
+                        date_int = Convert.ToInt32(date_string);
+                        if (type == "y")
+                        {
+                            if (date_int < 0 || date_int > DateTime.Now.Year)
+                            {
+                                Console.WriteLine("Unreal!");
+                            }
+                            else
+                            {
+                                break;
+                            }
+                        }
+                        else if (type == "m")
+                        {
+                            if (date_int < 1 || date_int > 12)
+                            {
+                                Console.WriteLine("Unreal!");
+                            }
+                            else
+                            {
+                                break;
+                            }
+
+                        }
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("Number!");
+                    }
+                }
+            }
+
+            //Console.WriteLine(date_int);
+
+            return date_int;
         }
     }
 }
